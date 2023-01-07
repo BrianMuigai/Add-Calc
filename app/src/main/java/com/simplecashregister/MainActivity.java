52164d7eaa987d955f9449c414f07e0b0d1e7e90
Brian Muigai <brianmuigai@Brians-MacBook-Pro.local>
2023-01-07 17:24:54 +0300

    create and intergrate Keyboard/NumPad layout

--- app/src/main/java/com/simplecashregister/MainActivity.java
+++ app/src/main/java/com/simplecashregister/MainActivity.java
@@ -1,22 +1,12 @@
 package com.simplecashregister;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
+import com.simplecashregister.customViews.KeyboardView;
 import com.simplecashregister.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
+
+        handleKeyboard();
+    }
+
+    private void handleKeyboard() {
+        KeyboardView keyboardView = binding.getRoot().findViewById(R.id.numPad);
+        keyboardView.setCustomListener(new KeyboardView.KeyboardListener() {
+            @Override
+            public void onAdd(String val) {
+                Log.e("Main Activity", "Add "+val);
+            }
+        });
     }
 
 
---
+++ app/src/main/java/com/simplecashregister/customViews/KeyboardView.java
@@ -0,0 +1,96 @@
+package com.simplecashregister.customViews;
+
+import android.content.Context;
+import android.text.Editable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import androidx.annotation.IdRes;
+
+import com.simplecashregister.R;
+
+public class KeyboardView extends FrameLayout implements View.OnClickListener {
+
+    private EditText text;
+    private KeyboardListener listener;
+
+    public KeyboardView(Context context) {
+        super(context);
+        init();
+    }
+
+    public KeyboardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public KeyboardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void setCustomListener(KeyboardListener listener) {
+        this.listener = listener;
+    }
+
+    private void init() {
+        inflate(getContext(), R.layout.keyboard, this);
+        initViews();
+    }
+
+    private void initViews() {
+        text = $(R.id.text_field);
+        $(R.id.t9_key_0).setOnClickListener(this);
+        $(R.id.t9_key_1).setOnClickListener(this);
+        $(R.id.t9_key_2).setOnClickListener(this);
+        $(R.id.t9_key_3).setOnClickListener(this);
+        $(R.id.t9_key_4).setOnClickListener(this);
+        $(R.id.t9_key_5).setOnClickListener(this);
+        $(R.id.t9_key_6).setOnClickListener(this);
+        $(R.id.t9_key_7).setOnClickListener(this);
+        $(R.id.t9_key_8).setOnClickListener(this);
+        $(R.id.t9_key_9).setOnClickListener(this);
+        $(R.id.t9_key_clear).setOnClickListener(this);
+        $(R.id.t9_key_add).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // handle number button click
+        if (v.getTag() != null && "number_button".equals(v.getTag())) {
+            text.append(((TextView) v).getText());
+            return;
+        }
+        switch (v.getId()) {
+            case R.id.t9_key_clear: {// handle backspace button
+                // delete one character
+                Editable editable = text.getText();
+                int charCount = editable.length();
+                if (charCount > 0) {
+                    editable.delete(charCount - 1, charCount);
+                }
+            }
+            break;
+            case R.id.t9_key_add: {  // handle add button
+                if (listener != null) listener.onAdd(text.getText().toString());
+                text.setText(null);
+            }
+            break;
+        }
+    }
+
+    public String getInputText() {
+        return text.getText().toString();
+    }
+
+    protected <T extends View> T $(@IdRes int id) {
+        return (T) super.findViewById(id);
+    }
+
+    public interface KeyboardListener {
+        void onAdd(String val);
+    }
+}
